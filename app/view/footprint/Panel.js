20e9c67ce2b33c65792e2a9247ee46582b25c93b
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2016-05-04 00:34:20 -0300

    Issue #6 - # 6 - a button that when clicked retrieve the tile that is in the current position and displays a small window with basic information was created. this window is not ready yet.

--- app/view/footprint/Panel.js
+++ app/view/footprint/Panel.js
@@ -8,23 +8,64 @@ Ext.define('Tile.view.footprint.Panel', {
         'Tile.view.footprint.FootprintController'
     ],
 
+    config: {
+        tileInfo: null
+    },
+
     initComponent: function () {
         var me = this;
 
         Ext.apply(this, {
-            hideFootprint: false,
+            //hideFootprint: false,
             auxTools: [
                 {
                     xtype: 'button',
                     tooltip: 'Tile Detail',
                     iconCls: 'x-fa fa-info',
-                    scope: this
-                    //handler: me.onDetail
+                    scope: this,
+                    handler: me.onDetail
                 }
             ]
         });
 
         me.callParent(arguments);
-    }
+    },
+
+    onDetail: function () {
+        var me = this, 
+            w = me.getTileInfo(),
+            tile;
+
+        tile = me.getTileByPosition();
+        
+        if (w) {
+            w.close();
+        }
+
+        w = me.createWindowDetail(tile);
+        w.show();
+
+        me.setTileInfo(w);
+    },
+
+    createWindowDetail: function (tile) {
+        var me = this;
+        return Ext.create('Ext.window.Window', {
+            title: tile.get('tli_tilename'),
+            closeAction:'destroy',
+            width: 180,
+            height: 150,
+            constrainHeader:true,
+            layout: 'fit',
+            autoScroll: true,
+            renderTo: me.body,
+            x: 50,
+            y: 50,
+            items:[
+            ]
+        });
+
+    },
+
 
 });
